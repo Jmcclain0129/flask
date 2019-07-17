42b17a21d43a07039b97e6a8def856f5ee7b9904
Jmcclain0129 <jmcclain0129@gmail.com>
2019-07-17 15:33:38 +0100

    added index page

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP', '127.0.0.1'),
+            port=int(os.environ.get('PORT', '8080')),
             debug=True)
